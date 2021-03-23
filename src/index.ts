7f0deba1558b52e2cd1843e8642064d571ee89e7
AbdessalemLetaief <abdesalemletaief@gmail.com>
2021-03-23 17:21:07 +0100

    fix: add constants

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './constants';
+export * from './ftp.service';
+export * from './nestjs-ftp.module'
+export * from './interfaces/file.interface'
+export * from './interfaces/ftp.interface'
