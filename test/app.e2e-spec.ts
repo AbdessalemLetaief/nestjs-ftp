15d0960b78501d9774cd5faa5f87d1157fd42033
AbdessalemLetaief <abdesalemletaief@gmail.com>
2021-03-23 17:08:10 +0100

    feat: initialize ftp module

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/nestjs-ftp.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
