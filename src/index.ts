c32436080d9dad900c78bc129256082632fa815f
AbdessalemLetaief <abdesalemletaief@gmail.com>
2021-04-29 11:36:39 +0100

    fix: export connection options interface

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './ftp.service';
 export * from './nestjs-ftp.module'
 export * from './interfaces/file.interface'
 export * from './interfaces/ftp.interface'
+export * from './interfaces/connection-options.interface'
